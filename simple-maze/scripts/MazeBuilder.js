8680ca91704360963d29b18667a5939709430635
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-02-20 19:50:34 +0900

    Update Maze generate algorithm

--- simple-maze/scripts/MazeBuilder.js
+++ simple-maze/scripts/MazeBuilder.js
@@ -21,26 +21,32 @@ class MazeBuilder {
         
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
-                
                 if (y == 0 || y + 1 == this.height ||
                     x == 0 || x + 1 == this.width) {
                     mazeTips[y][x] = Tip.TYPES.STEEL
                 } else if (y % 2 === 0 && x % 2 === 0) {
                     mazeTips[y][x] = Tip.TYPES.STEEL
-                    switch (Math.floor(Math.random() * 4)) {
-                    case 0:
-                        mazeTips[y][x + 1] = Tip.TYPES.STEEL
-                        break;
-                    case 1:
-                        mazeTips[y][x - 1] = Tip.TYPES.STEEL
-                        break;
-                    case 2:
-                        mazeTips[y + 1][x] = Tip.TYPES.STEEL
-                        break;
-                    case 3:
-                        mazeTips[y - 1][x] = Tip.TYPES.STEEL
-                        break;
+                    let nx = x
+                    let ny = y
+                    while (mazeTips[ny][nx] && mazeTips[ny][nx].style !== Tip.TYPES.FLOOR.style) {
+                        nx = x
+                        ny = y
+                        switch (Math.floor(Math.random() * 4)) {
+                        case 0:
+                            nx -= 1
+                            break;
+                        case 1:
+                            nx += 1
+                            break;
+                        case 2:
+                            ny -= 1
+                            break;
+                        case 3:
+                            ny += 1
+                            break;
+                        }
                     }
+                    mazeTips[ny][nx] = Tip.TYPES.STEEL
                 } else {
                     if (!mazeTips[y][x]) {
                         mazeTips[y][x] = Tip.TYPES.FLOOR
@@ -48,7 +54,7 @@ class MazeBuilder {
                 }
             }
         }
-            
+        
         for (let y = 0; y < mazeTips.length; y++) {
             const row = document.createElement('div')
             row.classList.add('row')
