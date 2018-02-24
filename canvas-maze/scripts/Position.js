835e7f10559c831efa8a995936d5f1893da72d6b
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-02-25 02:17:34 +0900

    Remove useless file and methods

--- canvas-maze/scripts/Position.js
+++ canvas-maze/scripts/Position.js
@@ -3,16 +3,4 @@ class Position {
         this.x = x
         this.y = y
     }
-    
-    add(x, y) {
-        this.x += x
-        this.y += y
-    }
-    
-    toStyle() {
-        return {
-            left: (this.x * 32 + 16) + 'px',
-            top: (this.y * 32 + 16) + 'px'
-        }
-    }
 }
